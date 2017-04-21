9dc41ea0a3e6a65929e5f4ae58d74aa7f15c946b
JW <jw@localhost.localdomain>
2017-04-21 13:07:06 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+import "log"
+import "fmt"
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+const content = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>Hello Monkey</Say>
+</Response>`
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/xml")
+	fmt.Fprint(w, content)
+}
