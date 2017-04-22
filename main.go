ea0ef2be7387a75e8e33d0c2e41bf5a5983eaeb6
JW <jw@localhost.localdomain>
2017-04-22 11:53:29 +0200

    With recording playback

--- main.go
+++ main.go
@@ -3,33 +3,32 @@ package horstwilio
 import (
 	"fmt"
 	"net/http"
-
-	"google.golang.org/appengine"
-	"google.golang.org/appengine/log"
 )
 
 func init() {
 	http.HandleFunc("/", handler)
 }
 
-const content = `<?xml version="1.0" encoding="UTF-8"?>
+const (
+	welcomeMsg = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
     <Say>Hello Gopher, how are you?</Say>
 	<Record timeout="5" />
 </Response>`
+	echoMsg = `<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Play>%s</Play>
+</Response>`
+)
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/xml")
 
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+	rec := r.FormValue("RecordingUrl")
+	if rec == "" {
+		fmt.Fprint(w, welcomeMsg)
 		return
 	}
 
-	c := appengine.NewContext(r)
-	for k, v := range r.PostForm {
-		log.Infof(c, "%s: %v", k, v)
-	}
-
-	fmt.Fprint(w, content)
+	fmt.Fprintf(w, echoMsg, rec)
 }
