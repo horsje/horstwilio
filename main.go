0a5cb9fa5edd2b70d9b03064125014a335289c85
JW <jw@localhost.localdomain>
2017-04-22 10:05:14 +0200

    Met logging

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package horstwilio
 import (
 	"fmt"
 	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/log"
 )
 
 func init() {
@@ -11,10 +14,22 @@ func init() {
 
 const content = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <Say>Hello Gopher</Say>
+    <Say>Hello Gopher, how are you?</Say>
+	<Record timeout="5" />
 </Response>`
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/xml")
+
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	c := appengine.NewContext(r)
+	for k, v := range r.PostForm {
+		log.Infof(c, "%s: %v", k, v)
+	}
+
 	fmt.Fprint(w, content)
 }
